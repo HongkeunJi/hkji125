28c2fd7c0f2c846c934d81c50bb5a9916b05eae9
Hongkeun Ji <hkji125@gmail.com>
2024-06-13 16:09:49 +0900

    Update widget.py

--- widget.py
+++ widget.py
@@ -32,7 +32,7 @@ def main():
     st.set_page_config(
         page_title="RAG Chat")
 
-    st.title("mySUNI RAG Chatbot")
+    st.title("국가연구과제 수행방법론 응답 RAG Chatbot")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -50,17 +50,27 @@ def main():
             key="model_selection"
         )
         uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx', 'pptx'], accept_multiple_files=True)
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        #openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         
         # 환경 변수 입력을 위한 UI 추가
-        langchain_api_key = st.text_input("LangChain API Key", key="langchain_api_key", type="password")
-        langchain_project = st.text_input("LangChain Project", key="langchain_project")
+        #langchain_api_key = st.text_input("LangChain API Key", key="langchain_api_key", type="password")
+        #langchain_project = st.text_input("LangChain Project", key="langchain_project")
         
-        process = st.button("Process")
-    
+        #process = st.button("Process")
+
+
+    # API 키를 환경변수로 설정
+    os.environ["OPENAI_API_KEY"] = "sk-proj-oh9VBlFkT5cckAvjPmgET3BlbkFJZ74YwLkbnMA76tsvQL2W"
+
     # 입력받은 환경변수로 설정
-    os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
-    os.environ["LANGCHAIN_PROJECT"] = langchain_project
+    #os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
+    #os.environ["LANGCHAIN_PROJECT"] = langchain_project
+
+    os.environ["LANGCHAIN_API_KEY"] = "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"
+    os.environ["LANGCHAIN_PROJECT"] = "pt-bumpy-regard-71"
+
+
+
 
     if process:
         if not openai_api_key or not langchain_api_key or not langchain_project:
