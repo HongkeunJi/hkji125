952f24fcd75f1e61719402341a50f0c3619cae73
Hongkeun Ji <hkji125@gmail.com>
2024-06-13 15:38:56 +0900

    Update widget.py

--- widget.py
+++ widget.py
@@ -50,11 +50,11 @@ def main():
             key="model_selection"
         )
         uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx', 'pptx'], accept_multiple_files=True)
-        openai_api_key = st.text_input("OpenAI API Key", key="sk-proj-oh9VBlFkT5cckAvjPmgET3BlbkFJZ74YwLkbnMA76tsvQL2W", type="password")
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         
         # 환경 변수 입력을 위한 UI 추가
-        langchain_api_key = st.text_input("LangChain API Key", key="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", type="password")
-        langchain_project = st.text_input("LangChain Project", key="pt-bumpy-regard-71")
+        langchain_api_key = st.text_input("LangChain API Key", key="langchain_api_key", type="password")
+        langchain_project = st.text_input("LangChain Project", key="langchain_project")
         
         process = st.button("Process")
     
