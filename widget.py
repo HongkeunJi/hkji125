c514fad12914c14f245faa4802ea8c3364660b1a
Hongkeun Ji <hkji125@gmail.com>
2024-06-14 08:55:24 +0900

    Update widget.py

--- widget.py
+++ widget.py
@@ -44,4 +44,58 @@ print(f"\n[metadata]\n{docs[50].metadata}\n")
 # splits = text_splitter.split_documents(docs)
 # len(splits)
 # splits[0]
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300, chunk_overlap=30)
+
+splits = text_splitter.split_documents(docs)
+
+len(splits)
+
+# 벡터스토어를 생성합니다.
+vectorstore = FAISS.from_documents(
+    documents=splits, embedding=OpenAIEmbeddings())
+
+# 뉴스에 포함되어 있는 정보를 검색하고 생성합니다.
+retriever = vectorstore.as_retriever(k=8)
+
+# template = """Use the following pieces of context to answer the question at the end. Please answer that you do not know anything that cannot be confirmed in the context below. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+# Context : {context}
+
+# Question : {question}
+# Answer in Korean:"""
+
+# from langchain_core.prompts import PromptTemplate
+
+# prompt = PromptTemplate(
+#     input_variables=["context", "question"],
+#     template=template,
+# )
+
+prompt = hub.pull("rlm/rag-prompt")
+prompt
+
+print(prompt.messages[0].prompt.template)
+
+# llm = ChatOpenAI(model_name="gpt-4-turbo-preview", temperature=0)
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+
+
+
+def format_docs(docs):
+    # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+# 체인을 생성합니다.
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+rag_chain.invoke("국가연구개발 과제 수행 시 연구활동비 정산 방법과 프로세스를 자세히 알려줘?")
+
 
