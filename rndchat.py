e01d46270fa43bec94152661ff2301e1a89fe2f6
Hongkeun Ji <hkji125@gmail.com>
2024-06-14 21:39:27 +0900

    Update rndchat.py

--- rndchat.py
+++ rndchat.py
@@ -3,116 +3,128 @@ import tiktoken
 from loguru import logger
 import os
 import tempfile
-import requests
+
 
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.chat_models import ChatOpenAI
 
-from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from langchain.memory import ConversationBufferMemory
 from langchain_community.vectorstores import FAISS
 
-# Langsmith api 환경변수 설정
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+# from streamlit_chat import message
+from langchain_community.callbacks import get_openai_callback
+from langchain.memory import StreamlitChatMessageHistory
 
-# 하드코딩된 LangChain API 키와 프로젝트 설정
-langchain_api_key = "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"
-langchain_project = "pt-bumpy-regard-71"
+# Langsmith api 환경변수 설정
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
+
+
 
 def main():
-    st.set_page_config(page_title="RAG Chat")
+    st.set_page_config(
+        page_title="RAG Chat")
 
-    st.title("국가연구과제 수행방법론 Chatbot")
+    st.title("국가연구과제 업무처리방법")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+        st.session_state.chat_history = None
 
     if "processComplete" not in st.session_state:
         st.session_state.processComplete = None
 
-    repo_owner = 'HongkeunJi'
-    repo_name = 'hkji125'
-    file_path = '국가연구개발사업_연구개발비_사용_기준_개정안_본문_전문.pdf'
-    branch = 'main'  # 브랜치 이름
-
-    # GitHub API URL
-    url = f'https://raw.githubusercontent.com/{repo_owner}/{repo_name}/{branch}/{file_path}'
-
-    # 파일 내용 가져오기
-    response = requests.get(url)
-    content = response.content
-
     with st.sidebar:
+        model_selection = st.selectbox(
+            "Choose the language model",
+            ("gpt-3.5-turbo", "gpt-4-turbo-preview", "gpt-4o"),
+            key="model_selection"
+        )
         uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx', 'pptx'], accept_multiple_files=True)
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-        model_selection = st.selectbox("Choose a model", ["gpt-4", "gpt-4o", "gpt-3.5-turbo"], index=1)  # 기본 선택을 "gpt-4o"로 설정
+        
+        # 환경 변수 입력을 위한 UI 추가
+        langchain_api_key = st.text_input("LangChain API Key", key="langchain_api_key", type="password")
+        langchain_project = st.text_input("LangChain Project", key="langchain_project")
         
         process = st.button("Process")
     
+    # 입력받은 환경변수로 설정
+    os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
+    os.environ["LANGCHAIN_PROJECT"] = langchain_project
+
     if process:
-        # API 키를 환경변수로 설정
-        os.environ["OPENAI_API_KEY"] = openai_api_key
-        os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
-        os.environ["LANGCHAIN_PROJECT"] = langchain_project
-        
-        # PDF 파일 로드. 파일의 경로 입력
-        local_loader = PyPDFLoader(content)
-        files_text2 = local_loader.load()
-        text_chunks = get_text_chunks(files_text2)
-        vectorstore = get_vectorstore(text_chunks)
+        if not openai_api_key or not langchain_api_key or not langchain_project:
+            st.info("Please add all necessary API keys and project information to continue.")
+            st.stop()
+        files_text = get_text(uploaded_files)
+        text_chunks = get_text_chunks(files_text)
+        vetorestore = get_vectorstore(text_chunks)
 
-        st.session_state.conversation = get_conversation_chain(vectorstore, openai_api_key, model_selection)
+        st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key, st.session_state.model_selection)
+
         st.session_state.processComplete = True
-    
+
     if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "assistant", "content": "안녕하세요! 국가연구과제 수행관련 챗봇입니다."}]
+        st.session_state['messages'] = [{"role": "assistant",
+                                         "content": "안녕하세요! 관련법령 파일을 업로드하시고, 궁금한 점을 물어보세요."}]
 
-    # Chat display
     for message in st.session_state.messages:
-        if message["role"] == "user":
-            st.markdown(f"**You:** {message['content']}")
-        else:
-            st.markdown(f"**Bot:** {message['content']}")
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-    # Chat input
-    query = st.text_input("Message to chatbot")
+    history = StreamlitChatMessageHistory(key="chat_messages")
 
-    if st.button("Send"):
-        if query:
-            st.session_state.messages.append({"role": "user", "content": query})
-            if st.session_state.conversation:
-                with st.spinner("Thinking..."):
-                    chain = st.session_state.conversation
-                    result = chain({"question": query})
-                    response = result['answer']
-                    st.session_state.messages.append({"role": "assistant", "content": response})
-                    source_documents = result['source_documents']
+    # Chat logic
+    if query := st.chat_input("Message to chatbot"):
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        with st.chat_message("assistant"):
+            chain = st.session_state.conversation
+
+            with st.spinner("Thinking..."):
+                result = chain({"question": query})
+                with get_openai_callback() as cb:
                     st.session_state.chat_history = result['chat_history']
+                response = result['answer']
+                source_documents = result['source_documents']
 
-                    st.markdown(f"**Bot:** {response}")
-                    with st.expander("참고 문서 확인"):
-                        for doc in source_documents:
-                            st.markdown(doc.metadata['source'], help=doc.page_content)
+                st.markdown(response)
+                with st.expander("참고 문서 확인"):
+                    for doc in source_documents:
+                        st.markdown(doc.metadata['source'], help=doc.page_content)
 
+        # Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        
 def tiktoken_len(text):
     tokenizer = tiktoken.get_encoding("cl100k_base")
     tokens = tokenizer.encode(text)
     return len(tokens)
 
+
 def load_document(doc):
+    # 임시 디렉토리에 파일 저장
     temp_dir = tempfile.gettempdir()
     file_path = os.path.join(temp_dir, doc.name)
 
+    # 파일 쓰기
     with open(file_path, "wb") as file:
-        file.write(doc.getbuffer())
+        file.write(doc.getbuffer())  # 파일 내용을 임시 파일에 쓴다
 
+    # 파일 유형에 따라 적절한 로더를 사용하여 문서 로드 및 분할
     try:
         if file_path.endswith('.pdf'):
             loaded_docs = PyPDFLoader(file_path).load_and_split()
@@ -121,11 +133,18 @@ def load_document(doc):
         elif file_path.endswith('.pptx'):
             loaded_docs = UnstructuredPowerPointLoader(file_path).load_and_split()
         else:
-            loaded_docs = []
+            loaded_docs = []  # 지원되지 않는 파일 유형
     finally:
-        os.remove(file_path)
+        os.remove(file_path)  # 작업 완료 후 임시 파일 삭제
 
     return loaded_docs
+
+def get_text(docs):
+    doc_list = []
+    for doc in docs:
+        doc_list.extend(load_document(doc))
+    return doc_list
+
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -136,6 +155,7 @@ def get_text_chunks(text):
     chunks = text_splitter.split_documents(text)
     return chunks
 
+
 def get_vectorstore(text_chunks):
     embeddings = HuggingFaceEmbeddings(
         model_name="jhgan/ko-sroberta-multitask",
@@ -145,12 +165,13 @@ def get_vectorstore(text_chunks):
     vectordb = FAISS.from_documents(text_chunks, embeddings)
     return vectordb
 
-def get_conversation_chain(vectorstore, openai_api_key, model_selection):
+
+def get_conversation_chain(vetorestore, openai_api_key, model_selection):
     llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=model_selection, temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         chain_type="stuff",
-        retriever=vectorstore.as_retriever(search_type='mmr', verbose=True),
+        retriever=vetorestore.as_retriever(search_type='mmr', verbose=True),
         memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
         get_chat_history=lambda h: h,
         return_source_documents=True,
@@ -158,6 +179,8 @@ def get_conversation_chain(vectorstore, openai_api_key, model_selection):
     )
 
     return conversation_chain
+
+
 
 if __name__ == '__main__':
     main()
