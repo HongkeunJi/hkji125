3ad51f425215748b6712963842a46c750ea2cecb
Hongkeun Ji <hkji125@gmail.com>
2024-06-14 20:29:15 +0900

    Update rndchat.py

--- rndchat.py
+++ rndchat.py
@@ -138,18 +138,6 @@ def tiktoken_len(text):
 
 
 def load_document(doc):
-    """
-    업로드된 문서 파일을 로드하고, 해당 포맷에 맞는 로더를 사용하여 문서를 분할합니다.
-
-    지원되는 파일 유형에 따라 적절한 문서 로더(PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader)를 사용하여
-    문서 내용을 로드하고 분할합니다. 지원되지 않는 파일 유형은 빈 리스트를 반환합니다.
-
-    Parameters:
-    - doc (UploadedFile): Streamlit을 통해 업로드된 파일 객체입니다.
-
-    Returns:
-    - List[Document]: 로드 및 분할된 문서 객체의 리스트입니다. 지원되지 않는 파일 유형의 경우 빈 리스트를 반환합니다.
-    """
     # 임시 디렉토리에 파일 저장
     temp_dir = tempfile.gettempdir()
     file_path = os.path.join(temp_dir, doc.name)
@@ -187,29 +175,6 @@ def get_text2(loader):
     return text
 
 def get_text_chunks(text):
-    """
-    주어진 텍스트 목록을 특정 크기의 청크로 분할합니다.
-
-    이 함수는 'RecursiveCharacterTextSplitter'를 사용하여 텍스트를 청크로 분할합니다. 각 청크의 크기는
-    `chunk_size`에 의해 결정되며, 청크 간의 겹침은 `chunk_overlap`으로 조절됩니다. `length_function`은
-    청크의 실제 길이를 계산하는 데 사용되는 함수입니다. 이 경우, `tiktoken_len` 함수가 사용되어 각 청크의
-    토큰 길이를 계산합니다.
-
-    Parameters:
-    - text (List[str]): 분할할 텍스트 목록입니다.
-
-    Returns:
-    - List[str]: 분할된 텍스트 청크의 리스트입니다.
-
-    사용 예시:
-    텍스트 목록이 주어졌을 때, 이 함수를 호출하여 각 텍스트를 지정된 크기의 청크로 분할할 수 있습니다.
-    이렇게 분할된 청크들은 텍스트 분석, 임베딩 생성, 또는 기계 학습 모델의 입력으로 사용될 수 있습니다.
-
-    주의:
-    `chunk_size`와 `chunk_overlap`은 분할의 세밀함과 처리할 텍스트의 양에 따라 조절할 수 있습니다.
-    너무 작은 `chunk_size`는 처리할 청크의 수를 불필요하게 증가시킬 수 있고, 너무 큰 `chunk_size`는
-    메모리 문제를 일으킬 수 있습니다. 적절한 값을 실험을 통해 결정하는 것이 좋습니다.
-    """
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=900,
         chunk_overlap=100,
@@ -220,24 +185,6 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    """
-    주어진 텍스트 청크 리스트로부터 벡터 저장소를 생성합니다.
-
-    이 함수는 Hugging Face의 'jhgan/ko-sroberta-multitask' 모델을 사용하여 각 텍스트 청크의 임베딩을 계산하고,
-    이 임베딩들을 FAISS 인덱스에 저장하여 벡터 검색을 위한 저장소를 생성합니다. 이 저장소는 텍스트 청크들 간의
-    유사도 검색 등에 사용될 수 있습니다.
-
-    Parameters:
-    - text_chunks (List[str]): 임베딩을 생성할 텍스트 청크의 리스트입니다.
-
-    Returns:
-    - vectordb (FAISS): 생성된 임베딩들을 저장하고 있는 FAISS 벡터 저장소입니다.
-
-    모델 설명:
-    'jhgan/ko-sroberta-multitask'는 문장과 문단을 768차원의 밀집 벡터 공간으로 매핑하는 sentence-transformers 모델입니다.
-    클러스터링이나 의미 검색 같은 작업에 사용될 수 있습니다. KorSTS, KorNLI 학습 데이터셋으로 멀티 태스크 학습을 진행한 후,
-    KorSTS 평가 데이터셋으로 평가한 결과, Cosine Pearson 점수는 84.77, Cosine Spearman 점수는 85.60 등을 기록했습니다.
-"""
     embeddings = HuggingFaceEmbeddings(
         model_name="jhgan/ko-sroberta-multitask",
         model_kwargs={'device': 'cpu'},
@@ -248,29 +195,7 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vetorestore, openai_api_key, model_selection):
-    """
-    대화형 검색 체인을 초기화하고 반환합니다.
 
-    이 함수는 주어진 벡터 저장소, OpenAI API 키, 모델 선택을 기반으로 대화형 검색 체인을 생성합니다.
-    이 체인은 사용자의 질문에 대한 답변을 생성하는 데 필요한 여러 컴포넌트를 통합합니다.
-
-    Parameters:
-    - vetorestore: 검색을 수행할 벡터 저장소입니다. 이는 문서 또는 데이터를 검색하는 데 사용됩니다.
-    - openai_api_key (str): OpenAI API를 사용하기 위한 API 키입니다.
-    - model_selection (str): 대화 생성에 사용될 언어 모델을 선택합니다. 예: 'gpt-3.5-turbo', 'gpt-4-turbo-preview'.
-
-    Returns:
-    - ConversationalRetrievalChain: 초기화된 대화형 검색 체인입니다.
-
-    함수는 다음과 같은 작업을 수행합니다:
-    1. ChatOpenAI 클래스를 사용하여 선택된 모델에 대한 언어 모델(LLM) 인스턴스를 생성합니다.
-    2. ConversationalRetrievalChain.from_llm 메소드를 사용하여 대화형 검색 체인을 구성합니다. 이 과정에서,
-       - 검색(retrieval) 단계에서 사용될 벡터 저장소와 검색 방식
-       - 대화 이력을 관리할 메모리 컴포넌트
-       - 대화 이력에서 새로운 질문을 생성하는 방법
-       - 검색된 문서를 반환할지 여부 등을 설정합니다.
-    3. 생성된 대화형 검색 체인을 반환합니다.
-    """
     llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=model_selection, temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
